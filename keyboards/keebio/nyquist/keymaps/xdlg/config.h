6bb240f11653b6652667d71b0149ae136594455c
xdlg <18530982+xdlg@users.noreply.github.com>
2022-06-05 08:39:28 +0200

    Add caps word

--- keyboards/keebio/nyquist/keymaps/xdlg/config.h
+++ keyboards/keebio/nyquist/keymaps/xdlg/config.h
@@ -17,7 +17,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-/* Select hand configuration */
 #define MASTER_LEFT
-
-#define ONESHOT_TAP_TOGGLE 5
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
