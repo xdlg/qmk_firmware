426975fd3b0b461fe9e4d8d4632eb317e6193252
xdlg <18530982+xdlg@users.noreply.github.com>
2025-03-13 09:13:46 +0100

    Add SYM layer tap

--- keyboards/keebio/nyquist/keymaps/xdlg/keymap.c
+++ keyboards/keebio/nyquist/keymaps/xdlg/keymap.c
@@ -5,6 +5,7 @@
 #define FUN 2
 
 #define FUN_SPC LT(FUN, KC_SPC)
+#define SYM_ENT LT(SYM, KC_ENT)
 
 enum custom_keycodes {
   ALT_TAB = SAFE_RANGE,
@@ -24,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | GUI  |      | Alt  |  FUN/Space  |    Enter    | SYM  | AltGr|AltTab| Ctrl |
+ * | Ctrl | GUI  |      | Alt  |  FUN/Space  |  SYM/Enter  |      | AltGr|AltTab| Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [DVO] = LAYOUT( \
@@ -32,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, \
   XXXXXXX, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, \
   KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, \
-  KC_LCTL, KC_LCMD, XXXXXXX, KC_LALT, FUN_SPC, FUN_SPC, KC_ENT,  KC_ENT,  MO(SYM), KC_RALT, ALT_TAB, KC_RCTL  \
+  KC_LCTL, KC_LCMD, XXXXXXX, KC_LALT, FUN_SPC, FUN_SPC, SYM_ENT, SYM_ENT, XXXXXXX, KC_RALT, ALT_TAB, KC_RCTL  \
 ),
 
 /* Symbols
