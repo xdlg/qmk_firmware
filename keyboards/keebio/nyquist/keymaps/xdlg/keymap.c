3ee6da4109a751c8a8f858579cd876f7fae6565e
xdlg <18530982+xdlg@users.noreply.github.com>
2025-04-17 08:54:51 +0200

    Remove SYM layer toggle

--- keyboards/keebio/nyquist/keymaps/xdlg/keymap.c
+++ keyboards/keebio/nyquist/keymaps/xdlg/keymap.c
@@ -5,7 +5,6 @@
 #define FUN 2
 
 #define FUN_SPC LT(FUN, KC_SPC)
-#define SYM_ENT LT(SYM, KC_ENT)
 
 #define ALT_1 RALT(KC_1)
 #define ALT_2 RALT(KC_2)
@@ -46,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |  GUI |      |  Alt |  FUN/Space  |  SYM/Enter  |  Alt |      |      | Ctrl |
+ * | Ctrl |  GUI |      |  Alt |  FUN/Space  |    Enter    |  SYM |      |      | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [DVO] = LAYOUT( \
@@ -54,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH, \
   XXXXXXX, KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_MINS, \
   KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT, \
-  KC_LCTL, KC_LCMD, XXXXXXX, KC_LALT, FUN_SPC, FUN_SPC, SYM_ENT, SYM_ENT, KC_LALT, XXXXXXX, XXXXXXX, KC_RCTL  \
+  KC_LCTL, KC_LCMD, XXXXXXX, KC_LALT, FUN_SPC, FUN_SPC, KC_ENT,  KC_ENT,  MO(SYM), XXXXXXX, XXXXXXX, KC_RCTL  \
 ),
 
 /* Symbols
